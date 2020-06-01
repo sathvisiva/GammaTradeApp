41a63e62ce5beddd24ada3a939594c65c9b2d180
Gaurav Singh <gauravsingh0109@gmail.com>
2020-06-01 20:46:32 +0530

    backtest

--- BackTest_module/BackTest_module.py
+++ BackTest_module/BackTest_module.py
@@ -1,6 +1,12 @@
 import sys
 sys.path.append('secret/')
-import test1
+import data_code
+import datetime
 
 print("this is a backtest module")
-test1.test_fn()
+
+start_data=int((datetime.datetime(2020,4,1,9,15)).timestamp())
+end_data=int((datetime.datetime(2020,5,1,15,30)).timestamp())
+data_code.fetch_data(start_data,end_data)
+
+print(data_code.get_data())
